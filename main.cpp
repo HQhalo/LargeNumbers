44ba0d8b6945edf86b87927c211d51ec0e349e45
tungtp <tungtp@users.noreply.github.com>
2019-03-27 01:40:31 +0700

    Update main.cpp
    đọc Xuất truy vấn

--- main.cpp
+++ main.cpp
@@ -3,5 +3,11 @@
 int main(int argc, char const *argv[])
 {
     /* code */
+	std::string s;
+
+	getline(std::cin,s);
+	//QInt::decToBin(s).PrintQInt();
+	std::cout << QInt::getToken(s);
+	system("pause");
     return 0;
 }
