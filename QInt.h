b6b56552d063d20f4a27b837c285be994bc04aaf
HAOQUANG <haoquangms@gmail.com>
2019-03-26 10:03:35 +0700

    Revert "Merge pull request #1 from HQhalo/branch_tam"
    This reverts commit 4d2e8c4ab2a774f24bb3cea40e5eabc254da594e, reversing
    changes made to fc83c65a0289d1950922fcd2bc972b446255651c.

--- QInt.h
+++ QInt.h
@@ -7,7 +7,6 @@ const char hexTable[16] = {'0', '1', '2', '3',
                       '8', '9', 'A', 'B',
                       'C', 'D', 'E', 'F'};
 
-
 class QInt
 {
 private:
@@ -15,17 +14,51 @@ private:
     unsigned int cell[4];
     
 public:
+    void scanQInt();
+    void PrintQInt();
+    
     static QInt decToBin(std::string str);
     static std::string binToDec(QInt x);
     static std::string binToHex(QInt x);
-    static std::string decToHex(std::string x);    
+    static std::string decToHex(std::string x);
 
-private:
-    bool getBit(const unsigned char &index);
-    void turnBitOn(const unsigned char &index);
-    void turnBitOff(const unsigned char &index);
+    QInt & operator + ( QInt const & other);
+    QInt & operator - ( QInt const & other);
+    QInt & operator * ( QInt const & other);
+    QInt & operator / ( QInt const & other);
 
-public:
-    QInt operator -();  
+    QInt & operator = ( QInt const &other);
+    bool operator < ( QInt const &other);
+    bool operator <= ( QInt const &other);
+    bool operator > ( QInt const &other);
+    bool operator >= ( QInt const &other);
+    bool operator == ( QInt const &other);
+    
+     QInt & operator = (const long long &other);
+    bool operator < (const long long &other);
+    bool operator <= (const long long &other);
+    bool operator > (const long long &other);
+    bool operator >= (const long long &other);
+    bool operator == (const long long &other);
+
+    QInt operator & ( QInt const &other);
+    QInt operator | ( QInt const &other);
+    QInt operator ^ ( QInt const &other);
+    QInt operator ~ ();
+
+    QInt operator & (const long long& other);
+    QInt operator | (const long long& other);
+    QInt operator ^ (const long long& other);
+
+    QInt operator << (const int &n);
+    QInt operator >> (const int &n);
+    //QInt operator rol (int n);
+   // QInt operator ror (int n);
+    
+
     QInt(/* args */);
+    QInt(std::string decNum);
+    ~QInt();
 };
+
+
