4f1333a48164563bb4731596a95fe7c501483dc2
Luu Chi Tam <40906287+chitam117119@users.noreply.github.com>
2019-03-26 20:52:59 +0700

    Update QInt.h

--- QInt.h
+++ QInt.h
@@ -13,27 +13,26 @@ class QInt
 private:
     /* data */
     unsigned int cell[4];
+  
+public:
+	std::string getToken(std::string Tokens);
+    void scanQInt();
+    void PrintQInt();
+
+public:
+    static QInt decToBin(std::string str);
+    static std::string binToDec(QInt x);
+    static std::string binToHex(QInt x);
+    static QInt hexToBin(const std::string &str);
+    static std::string decToHex(std::string x);    
 
 private:
     bool getBit(const unsigned char &index);
     void turnBitOn(const unsigned char &index);
     void turnBitOff(const unsigned char &index);
     void setBit(const unsigned char &index, const bool &value);
-  
-public:
-	std::string getToken(std::string Tokens);
-
-    void scanQInt();
-    void PrintQInt();
-    
-    static QInt decToBin(std::string str);
-    static std::string binToDec(QInt x);
-    static std::string binToHex(QInt x);
-    static std::string decToHex(std::string x);
 	
-
-
-
+public:
     QInt operator + ( QInt const & other);
     QInt operator - ( QInt const & other);
 	QInt  operator -();
