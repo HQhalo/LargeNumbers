9bd0f0ff92dc87dfe1e686b0c96a8922dbe7efac
tungtp <tungtp@users.noreply.github.com>
2019-03-26 09:32:03 +0700

    Update QInt.cpp

--- QInt.cpp
+++ QInt.cpp
@@ -1,6 +1,87 @@
 #include"QInt.h"
 
 
+
+QInt & operator + ( QInt const & other){
+	unsigned long long tmp = 0;
+	tmp += *this.cell[3];
+	tmp += other.cell[3];
+	cell[3] = tmp % (1 << 32);
+	tmp = tmp >> 32;
+
+	tmp += *this.cell[2];
+	tmp += other.cell[2];
+	cell[2] = tmp % (1 << 32);
+	tmp = tmp >> 32;
+
+	tmp += *this.cell[1];
+	tmp += other.cell[1];
+	cell[1] = tmp % (1 << 32);
+	tmp = tmp >> 32;
+
+	tmp += *this.cell[0];
+	tmp += other.cell[0];
+	cell[0] = tmp % (1 << 32);
+	tmp = tmp >> 32;
+}
+
+QInt & operator - ( QInt const & other){
+	QInt tmp = - other;
+	return *this + other;
+}
+
+QInt & operator * ( QInt const & other){
+	QInt ans;
+	for (int i = 3; i >= 0; i--)
+		unsigned long long tmp = 0;
+		int pos = i;
+		for (int j = 3; j >= 0; j--){
+			if (pos < 0)
+				break;
+
+			tmp += unsigned long long (cell[i]) * cell[j] + ans.cell[i];
+			ans.cell[i] = tmp % (1 <<32);
+			tmp >> 32;
+
+			pos--;
+		}
+}
+QInt & operator / ( QInt const & other){
+	QInt tmp = *this;
+
+	QInt de;
+	if (other > de) 
+		de = -other;
+	else 
+		de = other;
+
+	QInt zero ,num ;
+
+
+	if (* < 0)
+		num = ("-1");
+
+	for (int i = 0; i < 128; i++){
+		int bit_left = getbit(127);
+		tmp << 1;
+		num << 1;
+		num.setbit(0 , bit_left);
+
+
+		num = num + de;
+		if (num < zero){
+			tmp.setbit(0, 0);
+			num = num - de;
+		}
+			else 
+		tmp.setbit(0, 1);
+	}
+	return tmp;
+
+}
+
+
+
 bool QInt::operator == ( QInt const &other)
 {
     for(int i=0;i<4;i++)
