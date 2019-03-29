4ed49972673e02bada1e03fad28cb618ea982881
HAOQUANG <haoquangms@gmail.com>
2019-03-29 15:34:06 +0700

    add Qfloat addtion

--- Qfloat.cpp
+++ Qfloat.cpp
@@ -54,6 +54,71 @@ Qfloat Qfloat::operator * (Qfloat const & other) {
 }
 
 
+Qfloat Qfloat::operator + (Qfloat const & other)
+{
+    Qfloat re;
+
+    QInt number1(*this);
+    QInt number2(other);
+    QInt numberRe;
+
+    int exponent1 = this->getExponent();
+    int exponent2 = other.getExponent();
+    int exponent = 0 ;
+
+    bool sign1 = QInt.getBit(127);
+    bool sign2 = QInt.getBit(127);
+    bool sign;
+
+    if(exponent1 > exponent2)
+    {
+        int shift = exponent1- exponent2;
+        number2 = number2 >> shift ;
+        number2 = number1 ;
+    }
+    if(exponent1 < exponent2)
+    {
+        int shift = exponent2- exponent1;
+        number1 = number1 >> shift ;
+        number1 = number2 ;
+    }
+    exponent = exponent1;
+     
+    if(sign1 == sign2)
+    {
+        numberRe= number1 + number2;
+        sign = sign1;
+        if(numberRe.getBit(113)= true)
+        {
+            numberRe >> 1;
+            exponent ++ ;
+        }
+    }
+    else
+    {
+        numberRe = (number1 > number2 ) ? (number1 - number2 ) : ( number2- number1 );
+        sign = ( number1 > number2 ) ? sign1 : sign2;
+        
+        for(int i=112;i >= 0 ; i-- )
+        {
+            if(numberRe.getBit(112) == true)
+                break;
+            numberRe << 1; 
+            exponent -- ;       
+        }
+    }
+    
+    re.setBit(127,sign);
+    re.setExponent(exponent);
+    
+	for(int i=0 ; i < 112 ; i++)
+	{
+		re.setBit(i, (numberRe >> i) &1 ); 
+	}
+
+	return re;
+}
+
 
 
 
