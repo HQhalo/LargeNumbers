59cc9ae2eb5adebc1f144df8c0133446764af0fa
tungtp <tungtp@users.noreply.github.com>
2019-03-29 11:44:01 +0700

    Update Qfloat.cpp

--- Qfloat.cpp
+++ Qfloat.cpp
@@ -1,22 +1,64 @@
-QInt::QInt(){
+#include "QFloat.h"
+Qfloat::Qfloat(){
     cell[0] = cell[1] = cell[2] = cell[3] = 0;
 }
 
-bool QInt::getBit(const unsigned char &index){
+bool Qfloat::getBit(const unsigned char &index){
     return ((cell[3 - index / 32] >> (index % 32)) & 1);
 }
 
-void QInt::turnBitOn(const unsigned char &index){
+void Qfloat::turnBitOn(const unsigned char &index){
     cell[3 - index / 32] |= 1 << (index % 32);
 }
 
-void QInt::turnBitOff(const unsigned char &index){
+void Qfloat::turnBitOff(const unsigned char &index){
     cell[3 - index / 32] &= ~(unsigned int (1) << (index % 32));
 }
 
-void QInt::setBit(const unsigned char &index, const bool &value)
+void Qfloat::setBit(const unsigned char &index, const bool &value)
 {
     if (value) turnBitOn(index);
     else turnBitOff(index);
 }
+
+QInt Qfloat::convert() {
+	return QInt(cell);
+}
+
+int  Qfloat::getExponent() const {
+	Qfloat tmp = *this;
+	tmp.turnBitOff(127);
+	int ans = tmp.cell[0];
+	ans = ans >> 16;
+	return ans;
+}
+void Qfloat::setExponent(int Ex) {
+	unsigned int tmp = ((unsigned int)1 << 15) - 1;
+	tmp = tmp << 16;
+
+	(*this).cell[0] &= ~tmp;
+	cell[0] += Ex << 16;
+}
+
+
+
+Qfloat Qfloat::operator * (Qfloat const & other) {
+	int Ex1 = getExponent();
+	int Ex2 = other.getExponent();
+	int Ex = Ex1 + Ex2 - (1 << 16) - 1;
+
+	QInt tmp = ()
+
+	QInt A = (
+
+}
+
+
+
+
+
+
+
+
+
 
