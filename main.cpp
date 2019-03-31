037afce9ace7dfcde9355a9d7fd8ea85cb98181d
tungtp <tungtp@users.noreply.github.com>
2019-03-31 22:04:04 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,25 +25,44 @@ int main(int argc, char const *argv[])
 	std::cout << "\n";
 	std::cout << "\n";
 	std::cout << Qfloat::binToDec( x + y);
+	
 	*/
 
 	std::cout << "Test dectoBin\n";
 	std::cout << "Nhap so thap phan\n";
 	std::cin >> s;
 	Qfloat A = Qfloat::decToBin(s);
-
+	
 
 	std::cout << "Nhap so thap phan\n";
 	std::cin >> s;
 	Qfloat B = Qfloat::decToBin(s);
 
 
+<<<<<<< HEAD
 	std::cout << "ket qua phep *\n";
 	std::cout <<Qfloat::binToDec(A + B);
 
+=======
+	std::cout << "ket qua phep  *  \n";
+	std::cout <<Qfloat::binToDec(A * B);
+	std::cout << "\n";
+>>>>>>> tungtp_branch
 	//std::cout << Qfloat::binToDec(A);
 	
+	
+	
 
+	/*
+	std::cin >> s;
+	QInt a = QInt(s);
+	
+	std::cin >> s;
+	QInt b = QInt(s);
+
+	std::cout << QInt::binToDec(a / b);
+
+	*/
 
 	system("pause");
     return 0;
