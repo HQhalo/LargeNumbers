480fcdbfee3f24d8b4946eec97abbb37ec1fe856
tungtp <tungtp@users.noreply.github.com>
2019-04-01 23:51:48 +0700

    thêm tính năng test QInt

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include"QInt.h"
 #include "QFloat.h"
 
-
-void PrintBit(Qfloat a) {
+template
+<class T> void PrintBit(T a) {
 	for (int i = 0; i < 128; i++)
 		std::cout << a.getBit(127 - i);
 	std::cout << "\n";
@@ -11,38 +11,76 @@ void TestRange() {
 
 	Qfloat A,B;
 	std::string a = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
-	std::string b = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010";
+	std::string b = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
 	for (int i = 0; i < 128; i++)
 	{
 		A.setBit(127 - i, a[i] - '0');
 		B.setBit(127 - i, b[i] - '0');
 	}
-	A.setExponent((1 << 15) -1 );
+	A.setExponent((1 << 14) -1 + 1000 );
 
 	
-	B.setExponent(0);
-	std::string s = Qfloat::binToDec(A);
-	PrintBit(A);
+	B.setExponent((1 << 14) - 1 - 1000);
+	//std::string s = Qfloat::binToDec(A);
+	//PrintBit(A);
 	//std::cin >> s;
-	A = Qfloat::decToBin(s);
+	//A = Qfloat::decToBin(s);
 	/*std::cin >> s;
 	B = Qfloat::decToBin(s);
 	*/
 
-	//std::cout << Qfloat::binToDec(A) << "\n";
-	//std::cout << Qfloat::binToDec(B) << "\n";
-	//std::cout << Qfloat::binToDec(B * A) << "\n";
+	std::cout << Qfloat::binToDec(A) << "\n";
+	std::cout << Qfloat::binToDec(B) << "\n";
+	std::cout << Qfloat::binToDec(B * A) << "\n";
 	//PrintBit(B);
 	//PrintBit(A);
 
 	//A = B * A;
-	PrintBit(A);
+	//PrintBit(A);
+
+	system("pause");
+}
+void TestQInt() {
+	QInt A, B;
+	std::string a = "0000000000000000000000000100010010000000000000000000000000000000000000000000000000000000000000000000000000000000000001001010000";
+	std::string b = "00000000000101001000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000010000000000010";
+	for (int i = 0; i < 128; i++)
+	{
+		A.setBit(127 - i, (a[i] - '0' ));
+		B.setBit(127 - i, b[i] - '0');
+	}
+	
+
+	std::string s = QInt::binToDec(A);
+	//std::cout << s << "\n";
+	//PrintBit(A);
+	//PrintBit(B);
+	//PrintBit(A + B);
+	//std::cout << QInt::binToDec(A) << "\n";
+	//std::cout << QInt::binToDec(B) << "\n";
+	//std::cout << QInt::binToDec(B / A) << "\n";
+
+
+	std::cin >> s;
+	A = QInt::hexToBin(s);
+
+	std::cin >> s;
+	B = QInt::hexToBin(s);
+
+	std::cout << QInt::binToHex(A + B);
+
+	//std::cin >> s;
+	//A = QInt::decToBin(s);
+	/*std::cin >> s;
+	B = Qfloat::decToBin(s);
+	*/
 
 	system("pause");
 }
 int main(int argc, char const *argv[])
 {
 	TestRange();
+	//TestQInt();
 	return 0;
     /* code */
 	/*std::string s;
