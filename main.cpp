862672dab12f7221ecc2a7a4bfa79c07534e064a
HAOQUANG <haoquangms@gmail.com>
2019-04-01 23:40:18 +0700

    hgighjgjh

--- main.cpp
+++ main.cpp
@@ -2,76 +2,13 @@
 #include "QFloat.h"
 int main(int argc, char const *argv[])
 {
-    /* code */
 	std::string s;
-	// getline(std::cin,s);
+	getline(std::cin,s);
 	
-	// std::cout << QInt::getToken(s);
+	std::cout << QInt::getToken(s);
 
-	/*
-
-	Qfloat x = Qfloat();
-	Qfloat::scanQfloat(x);
-
-	std::cout << Qfloat::binToDec(x);
-
-	std::cout << "\n";
-	std::cout << "\n";
-
-	Qfloat y = Qfloat();
-	Qfloat::scanQfloat(y);
-	std::cout << Qfloat::binToDec(y);
-
-	std::cout << "\n";
-	std::cout << "\n";
-	std::cout << Qfloat::binToDec( x + y);
-	
-	*/
-
-	std::cout << "Test dectoBin\n";
-	std::cout << "Nhap so thap phan\n";
-	std::cin >> s;
-	Qfloat A = Qfloat::decToBin(s);
-	
-
-	// std::cout << "Test dectoBin\n";
-	// std::cout << "Nhap so thap phan\n";
-	// std::cin >> s;
-	// Qfloat A = Qfloat::decToBin(s);
-
-
-	 std::cout << "Nhap so thap phan\n";
-	 std::cin >> s;
-	Qfloat B = Qfloat::decToBin(s);
-
-
-
-	std::cout << "ket qua phep *\n";
-	std::cout <<Qfloat::binToDec(A + B);
-
-	// std::cout << "ket qua phep *\n";
-	// std::cout <<Qfloat::binToDec(A + B);
-
-
-	std::cout << "ket qua phep  *  \n";
-	std::cout <<Qfloat::binToDec(A * B);
-	std::cout << "\n";
-
-	//std::cout << Qfloat::binToDec(A);
 	
 	
-	
-
-	/*
-	std::cin >> s;
-	QInt a = QInt(s);
-	
-	std::cin >> s;
-	QInt b = QInt(s);
-
-	std::cout << QInt::binToDec(a / b);
-
-	*/
 
 	system("pause");
     return 0;
