88c3abc4bb6812ca84c8b9155164b7254109ffa2
tungtp <tungtp@users.noreply.github.com>
2019-04-01 21:28:47 +0700

    thêm hàm để test

--- main.cpp
+++ main.cpp
@@ -1,21 +1,43 @@
 #include"QInt.h"
 #include "QFloat.h"
 
+
+void PrintBit(Qfloat a) {
+	for (int i = 0; i < 128; i++)
+		std::cout << a.getBit(127 - i);
+	std::cout << "\n";
+}
 void TestRange() {
 
 	Qfloat A,B;
-	std::string a = "0000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000011";
-	std::string b = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010";
-	for (int i = 0; i < 127; i++)
+	std::string a = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+	std::string b = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010";
+	for (int i = 0; i < 128; i++)
 	{
 		A.setBit(127 - i, a[i] - '0');
-		std::cout << a[i] - '0';
 		B.setBit(127 - i, b[i] - '0');
 	}
+	A.setExponent((1 << 15) -1 );
 
-	std::cout << Qfloat::binToDec(A) << "\n";
-	std::cout << Qfloat::binToDec(B) << "\n";
-	std::cout << Qfloat::binToDec(A + B) << "\n";
+	
+	B.setExponent(0);
+	std::string s = Qfloat::binToDec(A);
+	PrintBit(A);
+	//std::cin >> s;
+	A = Qfloat::decToBin(s);
+	/*std::cin >> s;
+	B = Qfloat::decToBin(s);
+	*/
+
+	//std::cout << Qfloat::binToDec(A) << "\n";
+	//std::cout << Qfloat::binToDec(B) << "\n";
+	//std::cout << Qfloat::binToDec(B * A) << "\n";
+	//PrintBit(B);
+	//PrintBit(A);
+
+	//A = B * A;
+	PrintBit(A);
+
 	system("pause");
 }
 int main(int argc, char const *argv[])
